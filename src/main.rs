b4cd96cfd150dc6ec059fee3bee5ddc0a206b077
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-08 19:24:57 +0000

    feat: create the basic logic to obtain user input with the I/O crate

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use std::io::{self, Write};
+
+fn main() {
+    io::stdout()
+        .flush()
+        .expect("An error occurred while obtaining user input");
+
+    let mut user_input = String::new();
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("An error occurred while reading the user input");
+
+    print!("{}", user_input);
+}

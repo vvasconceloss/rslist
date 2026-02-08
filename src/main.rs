af90a7b24e37ac9e5a166c1854673519dad3e339
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-08 20:50:58 +0000

    feat: verify and control the flow according to the chosen command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+pub mod commands;
+
 use std::env;
+
+use crate::commands::Commands;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let command = Commands::from(&args[2].to_string());
+
+    match command {
+        Commands::Test => println!("Test Command"),
+        _ => panic!("Invalid command"),
+    }
 }

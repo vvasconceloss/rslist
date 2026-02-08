4c0516968f17a71115fc447fd887bc090f955dcf
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-08 19:52:22 +0000

    feat: replace input logic with use to use command line arguments (args)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-use std::io::{self, Write};
+use std::env;
 
 fn main() {
-    io::stdout()
-        .flush()
-        .expect("An error occurred while obtaining user input");
-
-    let mut user_input = String::new();
-
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("An error occurred while reading the user input");
-
-    print!("{}", user_input);
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }

0bb6a6a341d53a065830e1e7001e7ff27d003314
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-02-13 19:55:37 +0000

    feat: create an introduction function for the CLI

---
+++ src/introduction.rs
@@ -0,0 +1,11 @@
+pub fn print_usage() {
+    println!("RSList - Simple ToDo CLI");
+    println!("Use the following commands to interact:\n");
+
+    println!("Usage:");
+    println!("rslist <section> <command>\n");
+    println!("Commands:");
+    println!("test\tRun a test command");
+    println!("config\tShow CLI configuration");
+    println!();
+}
